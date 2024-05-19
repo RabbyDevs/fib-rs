ad6e86c1ac0fe89c849418d1067c4b6ae14e4e5d
Rabby <67389402+RabbyDevs@users.noreply.github.com>
2024-05-19 13:38:30 +0300

    possibly final version, made it as optimized as possible on CPU

--- src/main.rs
+++ src/main.rs
@@ -1,38 +1,29 @@
 use std::fs::File;
-use std::io;
+use std::io::{self, Read, Write};
 use std::time::SystemTime;
-use io::{stdin, stdout, Write, Read};
-use num::bigint::BigUint;
+use gmp::mpz::Mpz;
 
 fn new_parameter(ask_string: &str) -> String {
     println!("{}", ask_string);
     let mut parameter = String::new();
-
-    io::stdin()
-        .read_line(&mut parameter)
-        .expect("Failed to process input.");
+    io::stdin().read_line(&mut parameter).expect("Failed to process input.");
     parameter.trim_end().to_string()
 }
 
-// Get the final value
-fn fibonacci(nth: u128) -> BigUint {
-    fib(nth).0
-}
-
 // Head recursion
-fn fib(nth: u128) -> (BigUint, BigUint) {
+fn fib(nth: u128) -> (Mpz, Mpz) {
     // Break recursion if fib reaches zero
     if nth == 0 {
-        return (0u8.into(), 1u8.into());
+        return (Mpz::zero(), Mpz::one());
     }
     // Would number divide evenly by 2?
     let modulo_rem = nth % 2;
     // Subtract nth by modulo rem and divide by 2 to do floor division
-    let (a, b): (BigUint, BigUint) = fib((nth - modulo_rem) / 2u128);
+    let (a, b): (Mpz, Mpz) = fib((nth - modulo_rem) / 2);
 
     // Algorithm...
-    let c = &a * (&b * 2u8 - &a);
-    let d = &a * &a + &b * &b;
+    let c = &a * (&b * 2u64 - &a);
+    let d = &a.pow(2) + &b.pow(2);
 
     if modulo_rem == 1 {
         let summed = c + &d;
@@ -49,20 +40,28 @@ fn main() {
         .expect("Failed to parse input as a number");
 
     let start_time = SystemTime::now();
-    let fib_value = fibonacci(iterations);
-    let done_duration = SystemTime::now();
-    println!("done in {:#?}.", done_duration.duration_since(start_time).unwrap());
-    println!("printing to file...");
-    let mut file = File::create("./fib_number.txt").unwrap();
-    file.write("".to_string().as_bytes()).unwrap();
-    file.write(fib_value.to_string().as_bytes()).unwrap();
-    println!("file print completed in {:#?}, total time is {:#?}", SystemTime::now().duration_since(done_duration).unwrap(), SystemTime::now().duration_since(start_time).unwrap());
+    let fib_value = fib(iterations);
+    let computation_done_time = SystemTime::now();
+    println!("Computation done in {:#?}.", computation_done_time.duration_since(start_time).unwrap());
+
+    println!("Printing to file...");
+    let mut file = File::create("./fib_number.txt").expect("Failed to create file");
+    write!(file, "{}", fib_value.0).expect("Failed to write to file");
+
+    let file_done_time = SystemTime::now();
+
+    println!(
+        "File print completed in {:#?}, total time is {:#?}",
+        file_done_time.duration_since(computation_done_time).unwrap(),
+        file_done_time.duration_since(start_time).unwrap()
+    );
+
     pause();
 }
 
 fn pause() {
-    let mut stdout = stdout();
-    stdout.write(b"Press Enter to continue...").unwrap();
-    stdout.flush().unwrap();
-    stdin().read(&mut [0]).unwrap();
+    let mut stdout = io::stdout();
+    stdout.write_all(b"Press Enter to continue...").expect("Failed to write to stdout");
+    stdout.flush().expect("Failed to flush stdout");
+    io::stdin().read(&mut [0]).expect("Failed to read from stdin");
 }
